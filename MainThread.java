546a6ae1a18436a43e50265e50fe983327ddb9fc
longstl <jonnykhanh1102@gmail.com>
2018-03-30 12:00:24 +0700

    Add new file

---
+++ MainThread.java
@@ -0,0 +1,8 @@
+package newproject;
+
+public class MainThread {
+    public static void main(String[] args) {
+        Product product1 = new Product("A001", "Ao Dai", 20000, 10, "http://google.com", 2);
+        System.out.println(product1);
+    }
+}
